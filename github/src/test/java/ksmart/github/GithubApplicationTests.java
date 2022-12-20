47526b371bbf3de4ddf0cca96382b0ae5853546e
ECS <ECS@DESKTOP-048HU3O>
2022-12-20 11:44:09 +0900

    [github Project] spring boot 초기셋팅 author : ksmart45 황윤아

---
+++ github/src/test/java/ksmart/github/GithubApplicationTests.java
@@ -0,0 +1,13 @@
+package ksmart.github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
